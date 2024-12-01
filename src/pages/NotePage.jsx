814ffa7599eeb66d9faca9abff3469c867630da8
choiOV <121269364+choiOV@users.noreply.github.com>
2024-12-02 02:43:32 +0900

    Feat: useOnClickOutSide 커스텀 훅 생성해 클릭으로 모달 on/off 구현

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -1,18 +1,33 @@
-import * as S from "../styles/NotePageStyle";
-
-import plusOptionIcon from "../assets/images/plus-option-icon.png";
+import { useState, useRef } from "react";
 
 import Button from "../components/common/Button";
+import SelectMenu from "../components/common/SelectMenu";
 import NoteViewer from "../components/NoteViewer";
 
+import useOnClickOutside from "../hooks/useOnClickOutside";
+
+import { selectableCreations } from "../assets/data/selectableMenus";
+import plusOptionIcon from "../assets/images/plus-option-icon.png";
+
+import * as S from "../styles/NotePageStyle";
+
 function NotePage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  const handleOpenModal = () => setIsOpen(true);
+  const handleCloseModal = () => setIsOpen(false);
+
+  useOnClickOutside(modalRef, handleCloseModal);
+
   return (
     <S.NotePageLayout>
       <S.NoteLink to="/note/:id">
         <NoteViewer />
       </S.NoteLink>
       <S.NotePageItem type="option">
-        <Button image={plusOptionIcon} />
+        {isOpen && <SelectMenu ref={modalRef} menu={selectableCreations} />}
+        <Button image={plusOptionIcon} onClick={handleOpenModal} />
       </S.NotePageItem>
     </S.NotePageLayout>
   );
