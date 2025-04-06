a764565585f8a82595b71d43856f94dcf87adbea
choiOV <121269364+choiOV@users.noreply.github.com>
2025-04-07 03:07:12 +0900

    Test: E2E 테스트를 위한 Playwright 환경 설정

---
+++ playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 2,
+  reporter: "html",
+
+  use: {
+    baseURL: process.env.CLIENT_URL || "http://localhost:5173",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        headless: false,
+        launchOptions: {
+          args: ["--disable-blink-features=AutomationControlled", "--disable-popup-blocking"],
+        },
+      },
+    },
+  ],
+});
