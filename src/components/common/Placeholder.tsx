11f0eb00a10eca28f4b2fdb54a26f9682269c7e3
choiOV <121269364+choiOV@users.noreply.github.com>
2025-08-13 23:05:42 +0900

    Design: 플레이스홀더 컴포넌트 | 버튼 조정 안내문구 수정

--- src/components/common/Placeholder.tsx
+++ src/components/common/Placeholder.tsx
@@ -3,8 +3,8 @@ import * as S from "styles/components/PlaceholderStyle";
 function Placeholder() {
   return (
     <S.Layout>
-      “/”를 입력하거나 왼쪽 토글 버튼을 클릭해 텍스트를 수정하세요. 같은 버튼으로 블록 위치를 조정할
-      수도 있습니다.
+      “/”를 입력하거나 왼쪽 토글 버튼을 클릭해 텍스트를 수정하세요. 버튼을 드래그 앤 드롭해 블록
+      위치를 조정할 수도 있습니다.
     </S.Layout>
   );
 }
