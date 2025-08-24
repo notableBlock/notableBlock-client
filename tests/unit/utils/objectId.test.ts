c87e80c04ffdbb801af332d404aa558ea2022205
choiOV <121269364+choiOV@users.noreply.github.com>
2025-08-24 23:49:03 +0900

    Test: objectId | 24자리 16진수 문자열 반환 여부 테스트 추가

---
+++ tests/unit/utils/objectId.test.ts
@@ -0,0 +1,8 @@
+import objectId from "../../../src/utils/objectId";
+
+test("objectId는 24자리 16진수 문자열을 반환한다", () => {
+  const id = objectId();
+
+  expect(id).toHaveLength(24);
+  expect(id).toMatch(/^[0-9a-f]{24}$/);
+});
